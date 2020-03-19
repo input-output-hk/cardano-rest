30f699106a8994a42f5fba594e6e59d9cb00832c
KtorZ <matthias.benkort@gmail.com>
2020-03-19 20:55:40 +0100

    fix unit test not compiling

--- explorer-api/test/Test/Explorer/Web/Property/Types.hs
+++ explorer-api/test/Test/Explorer/Web/Property/Types.hs
@@ -1,23 +1,41 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Test.Explorer.Web.Property.Types
   ( tests
   ) where
 
-import           Explorer.Web.ClientTypes (adaToCCoin, cCoinToAda)
+import Cardano.Chain.Common
+    ( maxLovelaceVal )
+import Cardano.Db
+    ( Ada (..), word64ToAda )
+import Data.Word
+    ( Word64 )
+import Explorer.Web.ClientTypes
+    ( adaToCCoin, cCoinToAda )
+import Hedgehog
+    ( Gen, Property, discover )
 
-import           Hedgehog (Property, discover)
 import qualified Hedgehog as H
-
-import           Test.Property.Explorer.DB.Types (genAda)
-
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 
 prop_roundtrip_ada_to_ccoin :: Property
 prop_roundtrip_ada_to_ccoin =
   H.withTests 1000 . H.property $ do
     ada <- H.forAll genAda
     H.tripping ada adaToCCoin (Just . cCoinToAda)
+
+genAda :: Gen Ada
+genAda =
+    word64ToAda <$> genWord64Ada
+  where
+    genWord64Ada :: Gen Word64
+    genWord64Ada =
+      Gen.choice
+        [ Gen.word64 (Range.linear 0 maxLovelaceVal) -- Full range
+        , Gen.word64 (Range.linear 0 5000)           -- Small values
+        , Gen.word64 (Range.linear (maxLovelaceVal - 5000) maxLovelaceVal) -- Near max.
+        ]
 
 -- -----------------------------------------------------------------------------
 
