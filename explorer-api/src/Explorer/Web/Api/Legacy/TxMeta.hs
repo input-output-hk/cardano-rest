47d0bb7bf6387abf7fba8f7947626f03b9c4b416
dorranh <dorran.howell@tweag.io>
2020-12-14 11:22:33 +0100

    Run ormolu on new module

--- explorer-api/src/Explorer/Web/Api/Legacy/TxMeta.hs
+++ explorer-api/src/Explorer/Web/Api/Legacy/TxMeta.hs
@@ -1,36 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Explorer.Web.Api.Legacy.TxMeta
-  ( txMeta
-  ) where
+  ( txMeta,
+  )
+where
 
-import Cardano.Db
-    ( EntityField (..))
-import Control.Monad.IO.Class
-    ( MonadIO )
-
-import Database.Esqueleto
-    ( InnerJoin (..)
-    , Value (..)
-    , val
-    , from
-    , select
-    , on
-    , where_
-    , (==.)
-    , (^.)
-    )
-import Database.Persist.Sql
-    ( SqlPersistT )
+import Cardano.Db (EntityField (..))
+import Control.Monad.IO.Class (MonadIO)
 import qualified Data.Aeson as Aeson
+import Data.Maybe (mapMaybe)
 import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
-import Explorer.Web.Api.Legacy.Util ( textBase16Decode )
+import Database.Esqueleto
+  ( InnerJoin (..),
+    Value (..),
+    from,
+    on,
+    select,
+    val,
+    where_,
+    (==.),
+    (^.),
+  )
+import Database.Persist.Sql (SqlPersistT)
+import Explorer.Web.Api.Legacy.Util (textBase16Decode)
 import Explorer.Web.ClientTypes
-    ( CHash (..), CTxHash (..), CTxMeta (..))
-import Explorer.Web.Error
-    ( ExplorerError (..) )
-import Data.Maybe (mapMaybe)
+  ( CHash (..),
+    CTxHash (..),
+    CTxMeta (..),
+  )
+import Explorer.Web.Error (ExplorerError (..))
 
 -- | Gets the transaction metadata for a given transaction. Records
 -- which cannot be parsed to JSON will be ignored.
@@ -38,24 +37,26 @@ txMeta :: MonadIO m => CTxHash -> SqlPersistT m (Either ExplorerError [CTxMeta])
 txMeta = queryCTxMeta
 
 queryCTxMeta :: MonadIO m => CTxHash -> SqlPersistT m (Either ExplorerError [CTxMeta])
-queryCTxMeta cHash = do 
-    let (CTxHash (CHash hashTxt)) = cHash
-        hashBytes = textBase16Decode hashTxt
-    case hashBytes of 
-        Left e -> pure $ Left e 
-        Right hash -> do 
-            rows <- select . from $ \(meta `InnerJoin` tx) -> do 
-                        on (meta ^. TxMetadataTxId ==. tx ^. TxId)
-                        where_ (tx ^. TxHash ==. val hash)
-                        return $ meta ^. TxMetadataJson
-            pure $ Right $ mapMaybe (convert cHash) rows 
-    where 
-        convert :: CTxHash -> Value T.Text -> Maybe CTxMeta
-        convert chash (Value json) = let 
-            decoded = Aeson.decodeStrict $ encodeUtf8 json :: Maybe Aeson.Value 
-            in case decoded of 
-                Nothing -> Nothing 
-                Just v -> Just CTxMeta {
-                     ctmTxId = chash
-                   , ctmJSON = v 
-                }
+queryCTxMeta cHash = do
+  let (CTxHash (CHash hashTxt)) = cHash
+      hashBytes = textBase16Decode hashTxt
+  case hashBytes of
+    Left e -> pure $ Left e
+    Right hash -> do
+      rows <- select . from $ \(meta `InnerJoin` tx) -> do
+        on (meta ^. TxMetadataTxId ==. tx ^. TxId)
+        where_ (tx ^. TxHash ==. val hash)
+        return $ meta ^. TxMetadataJson
+      pure $ Right $ mapMaybe (convert cHash) rows
+  where
+    convert :: CTxHash -> Value T.Text -> Maybe CTxMeta
+    convert chash (Value json) =
+      let decoded = Aeson.decodeStrict $ encodeUtf8 json :: Maybe Aeson.Value
+       in case decoded of
+            Nothing -> Nothing
+            Just v ->
+              Just
+                CTxMeta
+                  { ctmTxId = chash,
+                    ctmJSON = v
+                  }
